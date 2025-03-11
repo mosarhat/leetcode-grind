ac16e4213f4d7e0f5dcec937e73c60c7c2eb6d50
mosarhat <mosarhat.01@gmail.com>
2025-03-11 01:30:46 -0400

    added stuff

---
+++ 1_arraysAndHashing/1 - Contains Duplicates (217, E)/solutions/217.py
@@ -0,0 +1,6 @@
+"""
+Name: Mohammed Sarhat
+The following is my code for the Contains Duplicate problem (217, Easy) from Leetcode.
+Outside this working directory, check the README.md for the problem statement, and my thought process. 
+Needless to say, I will include comments throughout the code that will carry the same intent; to show my thought process.
+"""
